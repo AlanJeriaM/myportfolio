37fea7f958e64f6e4b957a1d0c5591b80c152096
Alan Jeria Medina <alanjeria01@gmail.com>
2025-07-24 16:43:22 -0400

    update efectos de entrada para los diferentes componentes.

--- src/app/visitor/sections/habilidades/habilidades.component.ts
+++ src/app/visitor/sections/habilidades/habilidades.component.ts
@@ -1,5 +1,4 @@
-// habilidades.component.ts - VERSIÓN SIMPLIFICADA
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 
 interface Technology {
   name: string;
@@ -16,9 +15,12 @@ interface SkillCategory {
 @Component({
   selector: 'app-habilidades',
   templateUrl: './habilidades.component.html',
-  styleUrl: './habilidades.component.scss'
+  styleUrls: ['./habilidades.component.scss']
 })
-export class HabilidadesComponent {
+export class HabilidadesComponent implements AfterViewInit {
+
+  @ViewChild('skillsTitle') skillsTitle!: ElementRef<HTMLHeadingElement>;
+  animateTitle = false;
 
   skillCategories: SkillCategory[] = [
     {
@@ -26,22 +28,10 @@ export class HabilidadesComponent {
       icon: '🖥️',
       isExpanded: true,
       technologies: [
-        {
-          name: 'Angular',
-          logo: 'https://angular.io/assets/images/logos/angular/angular.svg'
-        },
-        {
-          name: 'Bootstrap',
-          logo: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png'
-        },
-        {
-          name: 'Tailwind CSS',
-          logo: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg'
-        },
-        {
-          name: 'PrimeNG',
-          logo: 'https://www.primefaces.org/wp-content/uploads/2018/05/primeng-logo.png'
-        }
+        { name: 'Angular', logo: 'https://angular.io/assets/images/logos/angular/angular.svg' },
+        { name: 'Bootstrap', logo: 'https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png' },
+        { name: 'Tailwind CSS', logo: 'https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg' },
+        { name: 'PrimeNG', logo: 'https://www.primefaces.org/wp-content/uploads/2018/05/primeng-logo.png' }
       ]
     },
     {
@@ -49,18 +39,9 @@ export class HabilidadesComponent {
       icon: '⚙️',
       isExpanded: true,
       technologies: [
-        {
-          name: 'Node.js',
-          logo: 'https://nodejs.org/static/images/logo.svg'
-        },
-        {
-          name: 'Express.js',
-          logo: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg'
-        },
-        {
-          name: 'AWS S3',
-          logo: 'https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg'
-        }
+        { name: 'Node.js', logo: 'https://nodejs.org/static/images/logo.svg' },
+        { name: 'Express.js', logo: 'https://www.vectorlogo.zone/logos/expressjs/expressjs-icon.svg' },
+        { name: 'AWS S3', logo: 'https://www.vectorlogo.zone/logos/amazon_aws/amazon_aws-icon.svg' }
       ]
     },
     {
@@ -68,14 +49,8 @@ export class HabilidadesComponent {
       icon: '🗄️',
       isExpanded: true,
       technologies: [
-        {
-          name: 'MySQL',
-          logo: 'https://www.vectorlogo.zone/logos/mysql/mysql-official.svg'
-        },
-        {
-          name: 'MongoDB',
-          logo: 'https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg'
-        }
+        { name: 'MySQL', logo: 'https://www.vectorlogo.zone/logos/mysql/mysql-official.svg' },
+        { name: 'MongoDB', logo: 'https://www.vectorlogo.zone/logos/mongodb/mongodb-icon.svg' }
       ]
     },
     {
@@ -83,14 +58,8 @@ export class HabilidadesComponent {
       icon: '📚',
       isExpanded: true,
       technologies: [
-        {
-          name: 'Git',
-          logo: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg'
-        },
-        {
-          name: 'GitHub',
-          logo: 'https://www.vectorlogo.zone/logos/github/github-icon.svg'
-        }
+        { name: 'Git', logo: 'https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg' },
+        { name: 'GitHub', logo: 'https://www.vectorlogo.zone/logos/github/github-icon.svg' }
       ]
     },
     {
@@ -98,25 +67,31 @@ export class HabilidadesComponent {
       icon: '🛠️',
       isExpanded: true,
       technologies: [
-        {
-          name: 'VS Code',
-          logo: 'https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg'
-        },
-        {
-          name: 'Postman',
-          logo: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg'
-        },
-        {
-          name: 'WordPress',
-          logo: 'https://www.vectorlogo.zone/logos/wordpress/wordpress-icon.svg'
-        },
-        {
-          name: 'WooCommerce',
-          logo: 'https://woocommerce.com/wp-content/themes/woo/images/logo-woocommerce@2x.png'
-        }
+        { name: 'VS Code', logo: 'https://www.vectorlogo.zone/logos/visualstudio_code/visualstudio_code-icon.svg' },
+        { name: 'Postman', logo: 'https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg' },
+        { name: 'WordPress', logo: 'https://www.vectorlogo.zone/logos/wordpress/wordpress-icon.svg' },
+        { name: 'WooCommerce', logo: 'https://woocommerce.com/wp-content/themes/woo/images/logo-woocommerce@2x.png' }
       ]
     }
   ];
+
+  ngAfterViewInit() {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          this.animateTitle = true;
+          observer.unobserve(this.skillsTitle.nativeElement); // 👈 solo una vez
+        }
+      },
+      {
+        threshold: 0.3 // 30% visible
+      }
+    );
+
+    if (this.skillsTitle?.nativeElement) {
+      observer.observe(this.skillsTitle.nativeElement);
+    }
+  }
 
   toggleCategory(index: number): void {
     this.skillCategories[index].isExpanded = !this.skillCategories[index].isExpanded;
